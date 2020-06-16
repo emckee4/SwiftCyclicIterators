86b2ec94673bef4c9491bff62fe65f7282b2ddf6
Evan McKee <emckee@gmail.com>
2020-06-16 13:38:49 -0700

    tweaked package

--- Package.swift
+++ Package.swift
@@ -5,16 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "CyclicIterators",
-//    platforms: [.iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "CyclicIterators",
             targets: ["CyclicIterators"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-# CyclicIterators
+# SwiftCyclicIterators
 
   
 
